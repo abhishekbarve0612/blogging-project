2961c538046865626d59a38da66828b8a243c969
Abhishek Barve <barveabhishek.ab@gmail.com>
2025-11-15 19:26:10 +0530

    Implement Apollo Client for GraphQL data fetching, refactor Home component to display posts, and remove default Next.js content in page.tsx; update apolloClient setup for direct instantiation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,58 @@
-import Image from "next/image";
+'use client'
+
+import { gql } from "@apollo/client";
+import { ApolloProvider, useQuery } from "@apollo/client/react";
+import apolloClient from "@/lib/apolloClient";
+import type { Post } from "@/lib/types";
 import styles from "./page.module.css";
 
-export default function Home() {
+const AllPosts = gql`
+    query {
+      posts {
+        id
+        title
+      }
+    }
+`
+
+function Posts() {
+  const { data, loading, error } = useQuery<any>(AllPosts)
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
+
+  if (error) {
+    return (
+      <div>
+        {error.message}
+      </div>
+    )
+  }
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+        {
+          data?.posts?.map((post: Post) => (
+            <li key={post.id}>
+              {post.title}
+            </li>
+          ))
+        }
       </main>
     </div>
   );
 }
+
+function Home() {
+  return (
+
+    <ApolloProvider client={apolloClient}>
+      <Posts />
+    </ApolloProvider>
+  )
+}
+
+export default Home;
--- lib/apolloClient.ts
+++ lib/apolloClient.ts
@@ -1,13 +1,11 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
 
-const createClient = () => {
-  return new ApolloClient({
-    link: new HttpLink({
-      uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
-      fetch,
-    }),
-    cache: new InMemoryCache(),
-  })
-}
+const apolloClient = new ApolloClient({
+  link: new HttpLink({
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_ENDPOINT,
+    fetch,
+  }),
+  cache: new InMemoryCache(),
+})
 
-export default createClient
+export default apolloClient
